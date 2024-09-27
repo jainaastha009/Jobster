973b1ed60b4459e981ccc994372181489913efae
jainaastha009 <jainaastha009@gmail.com>
2024-09-27 15:25:28 +0530

    changes

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -15,9 +15,16 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
+          A cutting-edge online platform designed to revolutionize the job search and recruitment experience. 
+          Jobster connects job seekers with potential employers, offering an intuitive interface that streamlines 
+          the application process. Our application features advanced search functionalities, allowing users to filter
+           opportunities by industry, location, and job type, ensuring that every candidate finds the right fit for their
+            skills and aspirations. Employers can easily post job openings, manage applications, and connect with top talent, 
+            enhancing their hiring process. With Jobster, we aim to bridge the gap between talent and opportunity, empowering 
+            individuals to pursue their career goals while providing businesses with the resources they need to build exceptional
+             teams.
+
+
           </p>
           <Link to='/register' className='btn btn-hero'>
             Login/Register
